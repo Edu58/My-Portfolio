fa57eb67726ff108fef4f9459e9ef78b6cec9dea
“Edu58” <edumuriithi58@gmail.com>
2022-09-18 10:29:55 +0300

    fix: clicking on EK results to 404

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,10 @@ const Navbar = () => {
             <header className="text-center text-white py-1 bg-green-500 font-semibold">Currently open to opportunities</header>
             <nav className="px-2 sm:px-4 shadow-lg" style={{ backgroundColor: "#1e202b" }}>
                 <div className="container flex flex-wrap justify-between items-center mx-auto pb-1">
-                    <a href="/" className="flex items-center px-3">
+                    <div className="flex items-center px-3">
                         <img src={eddyPNG} alt="edwin" className="rounded-full w-10 md:w-16 mr-2 pb-4" />
                         <span className="self-center text-2xl font-bold whitespace-nowrap text-rose-600">EK</span>
-                    </a>
+                    </div>
                     <button onClick={() => setNavbar(!navbar)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
